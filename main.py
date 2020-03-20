e7ecaeeee54b9fe55b1816797327ed8572bd136b
paul <accounts@paul-charlet.fr>
2020-03-20 17:43:47 +0100

    print debug

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
+print("import ok")
+
 servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
+print("setup ok")
+
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
+
+print("init ok")
+
 try:
   while True:
     p.ChangeDutyCycle(5)
     time.sleep(0.5)
     p.ChangeDutyCycle(7.5)
+    print("in cycle")
     time.sleep(0.5)
     p.ChangeDutyCycle(10)
     time.sleep(0.5)
