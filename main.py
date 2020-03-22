908bf4bbf1ba3a34aaa543e4c21b0beca8da78ea
paul <accounts@paul-charlet.fr>
2020-03-22 11:43:10 +0100

    adding wheels direction function

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ print("import ok")
 camera = PiCamera()
 camera.rotation = 180
 
-servoPIN = 17
+servoPINcam = 17
+servoPINdirection = 12
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(servoPINcam, GPIO.OUT)
+GPIO.setup(servoPINdirection, GPIO.OUT)
 
 print("setup ok")
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+pcam = GPIO.PWM(servoPINcam, 50) # GPIO cam for PWM with 50Hz
+pdirection = GPIO.PWM(servoPINdirection, 50) # GPIO direction for PWM with 50Hz
+pcam.start(2.5) # Initialization cam
+time.sleep(1)
+pdirection.start(2.5) # Initialization direction
 
 print("init ok")
 
-gpiofunction = GPIO.gpio_function(servoPIN)
-print(gpiofunction)
-p.ChangeDutyCycle(0)
+#gpiofunction = GPIO.gpio_function(servoPINcam)
+#print(gpiofunction)
+pcam.ChangeDutyCycle(0)
+pdirection.ChangeDutyCycle(0)
 
 def takePicture(fileName):
     camera.start_preview()
@@ -31,15 +37,24 @@ def takePicture(fileName):
 def getToAngle(angle):
     angle = int(angle)
     angle = (angle/180)*8+2
-    p.ChangeDutyCycle(angle)
+    pcam.ChangeDutyCycle(angle)
     time.sleep(1)
-    p.ChangeDutyCycle(0)
+    pcam.ChangeDutyCycle(0)
+
+def getToDirection(angle):
+    angle = int(angle)
+    angle = ((angle+90)/180)*8+2
+    pdirection.ChangeDutyCycle(angle)
+    time.sleep(1)
+    pdirection.ChangeDutyCycle(0)
 
 try:
   while True:
-      desiredAngle = input("Angle ? From 0 to 180")
-      getToAngle(desiredAngle)
+      desiredCamAngle = input("Angle cam ? From 0 to 180")
+      getToAngle(desiredCamAngle)
       takePicture('./pictures/image.jpg')
+      desiredDirection = input("Direction ? From -90 to 90")
+      getToDirection(desiredDirection)
 except KeyboardInterrupt:
-  p.stop()
+  pcam.stop()
   GPIO.cleanup()
