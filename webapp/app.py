cd6c10bb6b2d4a86ca118421ec5a1d128a2ec297
paul <accounts@paul-charlet.fr>
2020-03-25 17:33:48 +0100

    flask app form and picture return

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,15 @@
- from flask import Flask, render_template
-
+from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+app.secret_key = 'uieorwhcjdqwouiersdhdfsjk'
+@app.route('/', methods = ['GET','POST'])
 
-@app.route('/')
-def index():
+def controlRequest():
+    if request.method == 'POST':
+        camangle = request.form['camangle']
+        wheelangle = request.form['wheelangle']
+        move = request.form['move']
+        message = "angle caméra de " + camangle
+        flash(message)
     return render_template('index.html')
 
 if __name__ == '__main__':
