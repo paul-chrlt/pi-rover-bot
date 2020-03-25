a5abf65c7be1e418dacd9999d71c012ff4de49e9
paul <accounts@paul-charlet.fr>
2020-03-25 18:29:57 +0100

    app is not in debug mode

--- webapp/app.py
+++ webapp/app.py
@@ -17,4 +17,4 @@ def controlRequest():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, port=80, host='0.0.0.0')
